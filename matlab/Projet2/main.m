d8dcfda49bc61a155e0dfc02be2630aa40efe719
3700240 <admin@main3.fr>
2020-06-02 18:27:43 +0200

    integrateur toujours a modifier mais algo semble fonctionel !

--- matlab/Projet2/main.m
+++ matlab/Projet2/main.m
@@ -3,9 +3,11 @@ clear; clc; clf;
 T = 10;
 L = 4*pi;
 
-taxis = [0 T];  nt = 15000;
+taxis = [0 T];  nt = 1000;
 xaxis = [0 L];  nx = 20;
 vaxis = [-5 5]; nv = 20;
+
+ne = 100;  % Nombre de points pour le calcul du champs
 
 np = nx*nv; % Nombre de particules
 
@@ -13,7 +15,7 @@ alpha = 10^(-3);
 k = 0.5;
 
 g1 = @(X) 1+alpha*cos(k*X);
-g2 = @(V) 1/(sqrt(2*pi)) *  exp((-V.^2)./2) .* power(V,2); % Retirer le dernier membre pour l'amortissement Landau
+g2 = @(V) 1/(sqrt(2*pi)) *  exp((-V.^2)./2) ; %.* power(V,2); % Retirer le dernier membre pour l'amortissement Landau
 f0 = @(X,V) g2(V).*g1(X);
 
 %%%%%%%%%%%%%%%%%%%
@@ -26,7 +28,7 @@ x = x(1:nx); % Petite technique our ne pas avoir de doublons d'électrons sur le
 v = linspace(vaxis(1),vaxis(2),nv); hv=v(2)-v(1);
 
 
-x_E = linspace(xaxis(1),xaxis(2),100);
+x_E = linspace(xaxis(1),xaxis(2),ne); hE = x_E(2)-x_E(1);
 
 energie_elec = zeros(1,nt); % Pour stocker ||E(t)||
 
@@ -43,9 +45,11 @@ omega   = omega(:); % Matrice -> Vecteur
 
 omegalong = [omega;omega;omega]; % Version de omega compatible avec la périodicité
 
-rho = rho3(x_E,[X-L;X;X+L],omegalong,hx); % On duplique X 2 fois pour simuler la périodicité
-phi = tridiagsolver([1 -2 1],(1-rho)/(hx^-2)); 
-E = calcul_champ(phi,hx);
+rho = rho3(x_E,[X-L;X;X+L],omegalong,hx);
+
+phi = tridiagsolver([1 -2 1],(1-rho)/(hE^-2));
+    
+E = calcul_champ(phi,hE);
 
 energie_elec(1)=norm(E);
 
@@ -71,8 +75,11 @@ for step=2:nt %% L'etape 1 est la condition initiale
     %X = mod(X+0.01,L); %V = V + (rand(size(V))-0.5)/100;
     
     rho = rho3(x_E,[X-L;X;X+L],omegalong,hx);
-    phi = tridiagsolver([1 -2 1],(1-rho)/(hx^-2)); 
-    E = calcul_champ(phi,hx);
+    
+    phi = tridiagsolver([1 -2 1],(1-rho)/(hE^-2));
+    
+    E = calcul_champ(phi,hE);
+    
     energie_elec(step)=norm(E);
     
     EX = interp1(x_E,E,X,'spline');   % Valeur du champ à l'emplacement des particules
@@ -99,6 +106,13 @@ for step=2:nt %% L'etape 1 est la condition initiale
         title("Champ electrique");
         xlabel("x");
         ylabel("E");
+        
+        figure(3); % Energie electrique au cours du temps
+        plot(t,energie_elec);
+        xlim(taxis);
+        title("Energie electrique au cours du temps");
+        xlabel("t");
+        ylabel("E");
 
     end
     
@@ -110,6 +124,7 @@ end
 figure(3); % Energie electrique au cours du temps
 plot(t,energie_elec);
 title("Energie electrique au cours du temps");
+xlim(taxis);
 xlabel("t");
 ylabel("E");
 
