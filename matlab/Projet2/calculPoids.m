b70618e15e32dda52982d238ceb48cf100275a1a
3700240 <admin@main3.fr>
2020-06-02 17:26:21 +0200

    quasi ok

--- matlab/Projet2/calculPoids.m
+++ matlab/Projet2/calculPoids.m
@@ -8,8 +8,10 @@ b = 6*h*f(xi)';
 omega = (A\b)';
 end
 
-function [M] = tridiag(n)
+function [M] = tridiag(n) % Devrait etre renommee presquetridiag desormais
 %TRIDIAG Matrice tridiagonale de coef 1 4 1
 %   Hello World
 M = diag(4*ones(1,n)) + diag(ones(1,n-1),1) + diag(ones(1,n-1),-1);
+M(1,n)=1;
+M(n,1)=1;
 end
