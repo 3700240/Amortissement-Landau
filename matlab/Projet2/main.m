82614d0c6a19e44dc25ed46056dc5e582d9f63ee
3700240 <admin@main3.fr>
2020-06-03 12:35:41 +0200

    Eclaircissement du code

--- matlab/Projet2/main.m
+++ matlab/Projet2/main.m
@@ -27,7 +27,7 @@ x = linspace(xaxis(1),xaxis(2),nx+1);  hx=x(2)-x(1);
 x = x(1:nx); % Petite technique our ne pas avoir de doublons d'Ã©lectrons sur les bords
 v = linspace(vaxis(1),vaxis(2),nv); hv=v(2)-v(1);
 
-[XX,VV] = meshgrid(linspace(xaxis(1),xaxis(2),80),linspace(vaxis(1),vaxis(2),80));
+[XX,VV] = meshgrid(linspace(xaxis(1),xaxis(2),40),linspace(vaxis(1),vaxis(2),40));
 
 
 x_E = linspace(xaxis(1),xaxis(2),ne); hE = x_E(2)-x_E(1);
@@ -67,7 +67,7 @@ for step=2:nt %% L'etape 1 est la condition initiale
     E = calcul_champ(phi,hE);
     energie_elec(step)=norm(E);
     
-    EX = interp1(x_E,E,X,'spline');   % Valeur du champ a  l'emplacement des particules
+    EX = interp1(x_E,E,X,'spline');   % Valeur du champ aï¿½ l'emplacement des particules
     
     V = V - ht*EX;
     X = mod(X + V*ht,L);
@@ -118,7 +118,7 @@ for step=2:nt %% L'etape 1 est la condition initiale
         nexttile
         su = surf(XX,VV,FF);
         su.EdgeColor = 'none';
-        title("Densité de particules reconstruite");
+        title("Densitï¿½ de particules reconstruite");
         xlabel("x");
         ylabel("v");
         xlim(xaxis);
